de93e51baf1e4018c7a2732a6d4c0eef6c752474
golzi99 <golzi99@gmail.com>
2024-08-15 13:40:48 +0300

    15.08.2024 init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,90 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
 
 export default App;
+
+// type MoneyType = {
+//     banknots: string,
+//     value: number,
+//     number: string
+// }
+//
+// type ButtonMoneyType = "all" | "rubles" | "dollars"
+
+//    const [money, setMoney] = useState([
+//         {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+//         {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+//         {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+//         {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+//         {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+//         {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+//         {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+//         {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+//     ] as Array<MoneyType>)
+//
+//     const[filter, setFilter] = useState("all" as ButtonMoneyType)
+//
+//     let filtredMoney: Array<MoneyType> = money
+//
+//     if (filter === "rubles") {
+//         filtredMoney = money.filter(typeMoney => typeMoney.banknots === "RUBLS")
+//     } else if (filter === "dollars") {
+//         filtredMoney = money.filter(typeMoney => typeMoney.banknots === "Dollars")
+//     }
+//
+//     const onClickFilterMoneyHandler = (nameButton: ButtonMoneyType) => {
+//         setFilter(nameButton)
+//     }
+//
+//     return (
+//         <div className="App">
+//             <ul>
+//                 {filtredMoney.map((obj, index) => {
+//                     return (
+//                         <li key={index}>
+//                             <span> {obj.banknots}</span>
+//                             <span> {obj.value}</span>
+//                             <span> {obj.number}</span>
+//                         </li>
+//                     )
+//                 })}
+//             </ul>
+//             <button onClick={() => onClickFilterMoneyHandler("all")}>all</button>
+//             <button onClick={() => onClickFilterMoneyHandler("rubles")}>rubles</button>
+//             <button onClick={() => onClickFilterMoneyHandler("dollars")}>dollars</button>
+
+
+// const [money, setMoney] = useState([
+//     {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+//     {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+//     {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+//     {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+//     {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+//     {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+//     {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+//     {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+// ] as Array<MoneyType>)
+//
+// const[filter, setFilter] = useState("all" as ButtonMoneyType)
+//
+// let filtredMoney: Array<MoneyType> = money
+//
+// if (filter === "rubles") {
+//     filtredMoney = money.filter(typeMoney => typeMoney.banknots === "RUBLS")
+// } else if (filter === "dollars") {
+//     filtredMoney = money.filter(typeMoney => typeMoney.banknots === "Dollars")
+// }
+//
+// const onClickFilterMoneyHandler = (nameButton: ButtonMoneyType) => {
+//     setFilter(nameButton)
+//}
